12959dbf035f1e5ff072e47206236b77cb7fcef8
Teodor Todorov <WorkCoding@Teodors-MacBook-Pro.local>
2022-11-11 17:41:48 +0200

    Adding new Components and Installing React Icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png'
+
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+       <header>
+           <div className="container header__container">
+               <h5>Hello I'm</h5>
+               <h1>Teodor Todorov</h1>
+               <h5 className="text-light">Full-Stack Developer</h5>
+               <CTA />
+
+               <div className='me'>
+                   <img src={ME} alt="me" />
+               </div>
+
+               <a href="#contact" className='scroll__down'>Scroll Down</a>
+           </div>
+       </header>
     )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className='header_socials'>
+            <a href="https://linkedin.com" target="_blank"></a>
+            <a href="https://github.com" target="_blank"></a>
+            <a href="https://dribble.com" target="_blank"></a>
+        </div>
+    )
+}
+
+export default HeaderSocials
